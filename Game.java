ae35c928c6dbccdbd8a9fbd75b588ce4b5d7d0db
jbarcenas <jbarcenas@student-bmhs-la.org>
2023-02-03 12:35:25 -0800

    final

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Game {
 
     private String hidden;
@@ -10,6 +12,24 @@ public class Game {
     }
 
     public void run(){
-        System.out.println("hello");
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter in a 5 letter hidden word");
+        hidden = kb.nextLine();
+        while(!Objects.equals(guess, hidden)){
+            hint = "";
+            System.out.println("Enter in a 5 letter word");
+            guess = kb.nextLine();
+            for(int i = 0; i < hidden.length();i++){
+                if(hidden.substring(i,i+1).equals(guess.substring(i,i+1))){
+                    hint += guess.substring(i,i+1);
+                }else if(hidden.indexOf(guess.substring(i,i+1)) != -1){
+                    hint += "*";
+                }else{
+                    hint += "+";
+                }
+            }
+            System.out.println(hint);
+        }
+        System.out.println("You guessed it!");
     }
 }
